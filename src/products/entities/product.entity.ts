5e6e39f37bef4a80564072b94cff8c113753cb8f
Edu <eedumarquess@gmail.com>
2024-06-02 00:00:00 -0300

    FEAT: create product entity and update swagger

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,37 @@
-export class Product {}
+import { Product } from "@prisma/client";
+import { ApiProperty } from "@nestjs/swagger";
+
+export class ProductEntity implements Product {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  category: string;
+
+  @ApiProperty()
+  brand: string;
+
+  @ApiProperty()
+  volumeMilliliters: number;
+
+  @ApiProperty()
+  weightGrams: number;
+
+  @ApiProperty()
+  alcoholContent: number;
+
+  @ApiProperty()
+  price: number;
+
+  @ApiProperty()
+  stock: number;
+
+  @ApiProperty()
+  description: string;
+
+  @ApiProperty()
+  createdAt: Date;
+}
