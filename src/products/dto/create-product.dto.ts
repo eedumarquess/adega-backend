3182ee8c633f0a5e8eb2b00b4ba23d59f0da3896
Edu <eedumarquess@gmail.com>
2024-06-04 00:00:00 -0300

    FEAT: install class-validator and pipes in main.ts to better requests by users

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,30 +1,54 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsNumber, IsOptional, IsString, Max, MinLength } from "class-validator";
 
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(4)
   @ApiProperty({ required: true })
   name: string;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty({ required: true })
   category: string;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty({ required: true })
   brand: string;
 
+  @IsNumber()
+  @IsOptional()
+  @IsNotEmpty()
   @ApiProperty({ required: false, default: 0 })
   volumeMilliliters?: number = 0;
 
+  @IsNumber()
+  @IsOptional()
+  @IsNotEmpty()
   @ApiProperty({ required: false, default: 0 })
   weightGrams?: number = 0;
 
+  @IsNumber()
+  @IsOptional()
+  @IsNotEmpty()
+  @Max(100)
   @ApiProperty({ required: false, default: 0.0 })
   alcoholContent?: number = 0.0;
 
+  @IsNumber()
+  @IsNotEmpty()
   @ApiProperty({ required: true })
   price: number;
 
+  @IsNumber()
+  @IsNotEmpty()
   @ApiProperty({ required: true})
   stock: number;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty({ required: true })
   description: string;
 }
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,8 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { 
+  Controller, 
+  Get, 
+  Post, 
+  Body, 
+  Patch, 
+  Param, 
+  Delete, 
+  ParseIntPipe
+} from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { ProductEntity } from './entities/product.entity';
 
 @Controller('products')
@@ -24,19 +33,22 @@ export class ProductsController {
 
   @Get(':id')
   @ApiOkResponse({ type: ProductEntity })
-  findOne(@Param('id') id: string) {
-    return this.productsService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.findOne(id);
   }
 
   @Patch(':id')
   @ApiOkResponse({ type: ProductEntity })
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(+id, updateProductDto);
+  update(
+    @Param('id', ParseIntPipe) id: number, 
+    @Body() updateProductDto: UpdateProductDto
+  ) {
+    return this.productsService.update(id, updateProductDto);
   }
 
   @Delete(':id')
   @ApiOkResponse({ type: ProductEntity })
-  remove(@Param('id') id: string) {
-    return this.productsService.remove(+id);
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.remove(id);
   }
 }
